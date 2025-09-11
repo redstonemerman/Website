43ee984d2f3cbe4bf02147f298da67a988384e3f
redstonemerman <jpkfilmers@gmail.com>
2025-09-11 13:49:55 -0500

    update

--- redscript.js
+++ redscript.js
@@ -1,17 +1,34 @@
-const texas = document.querySelector('#texasbox');
-const tx = document.querySelector('#tx');
-
 document.querySelector('.bgvid').playbackRate = 0.5;
+
+const texas = document.querySelector('#texasbox');
+const cali = document.querySelector('#calibox');
+const ca = document.querySelector('#ca');
+const x = document.querySelector('.x');
+const states = [texas, cali];
 
 const openTexas = () => {
     texas.classList.add('active')
 };
 
+const openCali = () => {
+    cali.classList.add('active')
+};
+
+const closeState = () => {
+    texas.classList.remove('active');
+    cali.classList.remove('active');
+};
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape'){
+    states.forEach(states => states.classList.remove('active'));
+    }
+});
+
 tx.addEventListener('click', openTexas);
 tx.addEventListener('touchstart', openTexas);
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && texas.classList.contains('active')){
-        texas.classList.remove('active')
-    }
-})
+ca.addEventListener('click', openCali);
+ca.addEventListener('touchstart', openCali);
+
+x.addEventListener('click',closeState);
