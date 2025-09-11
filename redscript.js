24bf8ae00f98d347c67d3ec0b3aa736398b2317e
redstonemerman <jpkfilmers@gmail.com>
2025-09-11 00:11:22 -0500

    update

--- redscript.js
+++ redscript.js
@@ -1,19 +1,14 @@
 const texas = document.querySelector('#texasbox');
-const tximg = document.querySelector('.texas');
 const tx = document.querySelector('#tx');
 
 document.querySelector('.bgvid').playbackRate = 0.5;
 
-tx.addEventListener('click', () => {
+const openTexas = () => {
     texas.classList.add('active')
-})
+};
 
-texas.addEventListener('click', (e) => {
-    if (e.target === tximg){
-        return;
-    }
-    texas.classList.remove('active')
-})
+tx.addEventListener('click', openTexas);
+tx.addEventListener('touchstart', openTexas);
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && texas.classList.contains('active')){
