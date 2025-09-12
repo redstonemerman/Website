6d4f58c60998dadf7fe4d08ae2bf1e1f0029f8af
redstonemerman <jpkfilmers@gmail.com>
2025-09-12 00:58:13 -0500

    update

--- redscript.js
+++ redscript.js
@@ -1,12 +1,12 @@
 document.querySelector('.bgvid').playbackRate = 0.5;
-
+// CONSTANTS
 const texas = document.querySelector('#texasbox');
 const cali = document.querySelector('#calibox');
 const ca = document.querySelector('#ca');
 const x = document.querySelectorAll('.x');
-
 const states = [texas, cali];
 
+// OPEN/CLOSE STATE LIGHTBOXES
 const openTexas = () => {
     texas.classList.add('active')
 };
@@ -24,7 +24,7 @@ document.addEventListener('keydown', (e) => {
     states.forEach(states => states.classList.remove('active'));
     }
 });
-
+// CLICK LISTENERS TO OPEN STATE LIGHTBOXES
 tx.addEventListener('click', openTexas);
 tx.addEventListener('touchstart', openTexas);
 
@@ -32,3 +32,17 @@ ca.addEventListener('click', openCali);
 ca.addEventListener('touchstart', openCali);
 
 x.forEach(x => x.addEventListener('click',closeState));
+
+// CALCULATE SCROLLING DISTANCE
+function updateScrollVariable() {
+  const scroll = document.documentElement.scrollHeight - window.innerHeight;
+  document.documentElement.style.setProperty('--scroll', `${scroll}%`);
+}
+
+window.addEventListener('load', updateScrollVariable);
+
+let resizeTimeout;
+window.addEventListener('resize', () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(updateScrollVariable, 100);
+});
